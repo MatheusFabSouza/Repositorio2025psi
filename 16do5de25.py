3c949ac74b181435f408cf91ef05fa44f0f859bb
MatheusFSouza <matheusfab1202@gmail.com>
2025-05-16 17:47:47 -0300

    Adicionando pesquisa

--- 16do5de25.py
+++ 16do5de25.py
@@ -1,2 +1,7 @@
 import requests
+
 print("meu programa não vai que vai") 
+
+nota = input("numa escala de 0 a 1253, como voce avalia a relevancia desse projeto inexistente?")
+
+print(f"esse projeto tem uma relevancia de {nota}")
